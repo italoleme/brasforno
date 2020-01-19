b8181e4557139e15233ee05a644b492e70ac76d5
unknown <italo@codeby.com.br>
2020-01-19 11:27:36 -0300

    Update INIT

---
+++ gulpfile.js
@@ -0,0 +1,120 @@
+var gulp = require('gulp'),
+    ejs = require('gulp-ejs'),
+    rename = require('gulp-rename'),
+    plumber = require('gulp-plumber'),
+    autoprefixer = require('gulp-autoprefixer'),
+    concat = require('gulp-concat'),
+    del = require('del'),
+    uglify = require('gulp-uglify'),
+    cleanCSS = require('gulp-clean-css'),
+    sass = require('gulp-sass'),
+    connect = require('gulp-connect'),
+    fs = require('fs'),
+    image = require('gulp-image');
+
+gulp.task('ejs', done => {
+    var jsonData01 = './snow-store.json',
+            json01 = JSON.parse( fs.readFileSync( jsonData01,'utf8' ) );
+
+    for( var i = 0; i < json01.length; i++ ) {
+        var fileName = json01[i].id.replace(/ +/g, '-').toLowerCase();
+
+        gulp.src('src/templates/**/product.ejs')
+            .pipe(ejs({
+                jData01:json01[i]
+            },))
+
+            .pipe(rename(fileName + '.html'))
+            .pipe(gulp.dest('dist/templates'));
+    }
+    done();
+});
+
+gulp.task('html-ejs',function(){
+    gulp.src(['src/templates/**/*.{ejs,html}', '!src/templates/**/product.ejs'])
+        .pipe(plumber({
+            handleError: function (err) {
+                console.log(err);
+                this.emit('end');
+            }
+        }))
+        .pipe(ejs({ msg: 'Hello Gulp!'}, {}, { ext: '.html' }))
+        .pipe(gulp.dest('dist/templates'))
+});
+
+gulp.task('connect', function() {
+    connect.server({
+      root: 'dist',
+      livereload: true
+    });
+});
+
+gulp.task('image', function () {
+    gulp.src(['src/image/product/*','src/image/banner/*'])
+      .pipe(image())
+      .pipe(gulp.dest('dist/image/'));
+});
+
+gulp.task('styles', function () {
+    gulp.src(['src/styles/**/*.{scss,sass,css}'])
+        .pipe(plumber({
+            errorHandler: function (error) {
+                console.log(error.message);
+                this.emit('end');
+            }
+        }))
+        .pipe(sass())
+        .pipe(autoprefixer('last 2 versions'))
+        .pipe(rename({
+            prefix: 'bra-'
+        }))
+        .pipe(gulp.dest('dist/css/'))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('dist/css/'))
+});
+
+gulp.task('scripts', function () {
+    return gulp.src('src/javascript/**/*.js')
+        .pipe(plumber({
+            errorHandler: function (error) {
+                console.log(error.message);
+                this.emit('end');
+            }
+        }))
+        //.pipe(concat('main.js'))
+        .pipe(rename({
+            prefix: 'bra-'
+        }))
+        .pipe(gulp.dest('dist/javascript/'))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        //remove todos os console.log
+        //.pipe(uglify({ compress: { drop_console: true } }).on('error', console.error))
+        .pipe(uglify().on('error', console.error))
+        .pipe(gulp.dest('dist/javascript/'))
+});
+
+gulp.task('run', ['clean', 'styles', 'scripts', 'ejs', 'html-ejs']);
+
+gulp.task('watch', function(){
+  gulp.watch("src/styles/**/*.{scss,sass,css}", ['styles']);
+  gulp.watch("src/javascript/**/*.js", ['scripts']);
+  gulp.watch("src/templates/**/product.ejs", ['ejs']);
+  gulp.watch("src/templates/**/*.{ejs,html}", ['html-ejs']);
+});
+
+// Clean up
+gulp.task('clean', function() {
+  return del(['dist/', '.sass-cache/']);
+});
+
+// gulp dist
+gulp.task('dist', ['clean'], function() {
+  gulp.start('styles', 'scripts', 'ejs', 'html-ejs', 'image');
+});
+
+gulp.task('default', ['run', 'watch', 'connect']);
---
+++ src/javascript/main.js
@@ -0,0 +1,56 @@
+var tabletWidth = 768;
+var isMobileWidth = window.matchMedia("(max-width: " + (tabletWidth - 1) + "px)").matches;
+
+var bras = {
+    init: function() {
+        bras.hover();
+        bras.nav();
+        bras.bannerPrincipal();
+        bras.mobileNav();
+        bras.closed();
+    },
+    hover: function() {
+        var _isopen = '_is-open';
+        $('._bra-nav>li').hover(function() {
+            $(this).addClass(_isopen);
+        },
+        function() {
+            $(this).removeClass(_isopen); 
+        }
+        );
+    },
+    nav: function(){
+        $('._bra-burguer').on('click', function(){
+            if(!$(this).hasClass('_active')){
+                $(this).addClass('_active');
+                $(this).parents('header').find('nav').addClass('_active');
+            }else{
+                $(this).removeClass('_active');
+                $(this).parents('header').find('nav').removeClass('_active');
+            }
+        });
+           
+    },
+    bannerPrincipal: function(){
+        $('._bra-banner-principal').slick({
+            arrows: false,
+            dots: true
+        });
+    },
+    mobileNav: function(){
+        if(isMobileWidth){
+            $('._bra-nav .link').on('click', function(e){
+                e.preventDefault();
+            });
+        }
+    },
+    closed: function(){
+        $('._bra-closed').on('click', function(){
+            $(this).parents('li').removeClass('_is-open');
+        })        
+    }
+}
+
+$(document).ready(function() {
+    bras.init();
+});
