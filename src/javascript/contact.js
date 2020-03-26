2fd135817e8aae1e11093d1d2b969f4150905309
Ítalo Leme <italo@codeby.com.br>
2020-03-25 22:04:54 -0300

    Update Contact

---
+++ src/javascript/contact.js
@@ -0,0 +1,12 @@
+var brasContact = {
+    init: function(){
+        brasContact.form();
+    },
+    form: function(){
+        
+    }
+}
+
+$(document).ready(function(){
+    brasContact.init();
+});
