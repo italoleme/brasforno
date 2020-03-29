2799554cfcd45abdf5d18d3b9312e62827077252
Ítalo Leme <italo@codeby.com.br>
2020-03-29 13:36:50 -0300

    Update Header link

--- src/javascript/contact.js
+++ src/javascript/contact.js
@@ -6,15 +6,23 @@ var brasContact = {
         $('._bra-submit').on('click', function(e){
             e.preventDefault();
             let form = [];
-            
+        
             $(this).parent().find("._bra-val").each(function(i) {
                  let name = $(this).attr('name'),
                      val = $(this).val();
-                     form.push(name + ' : ' + val);         
+                     form.push(name + ' : ' + val);     
+                 if($(this).is(':empty')){
+                    $(this).parent().addClass('_bra-error')
+                 }
+
              });
         
             console.log(JSON.stringify(form))
-         });
+        });
+
+        $("._bra-val").on('click', function(){
+            $(this).parent().removeClass("_bra-error");
+        });
     }
 }
 
